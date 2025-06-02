ab32ac437058dac8202ab3f4d2e44c8962359da9
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-06-02 19:57:46 -0300

    Initial IIFE for the Gameboard module

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World!")
+
+// Initial Gameboard module
+const Gameboard = (() => {
+
+    // Initialize board fields
+    let board = [ "", "", "", "", "", "", "", "", "" ];
+
+    // Get current board state
+    const getBoardState = () => board;
+
+    // Set X/O into the specified position
+    const setPlayerMark = (index, mark) => {
+        if (board[index] === "") {
+            board[index] = mark;
+        };
+    };
+
+    // Board reset routine
+    const resetBoardState = () => {
+        board = [ "", "", "", "", "", "", "", "", "" ];
+    };
+
+    // Module output values
+    return {
+        getBoardState,
+        setPlayerMark,
+        resetBoardState
+    };
+
+})();
